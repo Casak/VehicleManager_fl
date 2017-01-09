77edcc8c45fbd5e16bf004a6849d4336a0d9e3ea
Casak <casak123@gmail.com>
2017-01-09 02:18:28 +0200

    Register receiver for sending data to server.

--- app/src/main/java/com/ocr/DefectReportActivity.java
+++ app/src/main/java/com/ocr/DefectReportActivity.java
@@ -1,12 +1,32 @@
 package com.ocr;
 
 import android.app.Activity;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 public class DefectReportActivity extends Activity{
+    private InternetConnectionReceiver receiver;
+    private CompoundButton mSwitchExteriorFluidLeaks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.defect_report_activity);
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        receiver = InternetConnectionReceiver.getInstance(getApplicationContext());
+        registerReceiver(receiver, filter);
+        receiver.setNeedToUpload(true);
+
+        mSwitchExteriorFluidLeaks = (CompoundButton) findViewById(R.id.exterior_fluid_leaks);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
     }
 }
